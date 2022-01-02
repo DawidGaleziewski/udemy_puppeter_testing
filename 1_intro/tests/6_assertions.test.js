9dfec8d6d927e498b53fb2101b567c6a616bafa8
Dawid Gałęziewski <dawid.galeziewski.dev@gmail.com>
2022-01-02 18:52:02 +0100

    saving nodes

--- 1_intro/tests/6_assertions.test.js
+++ 1_intro/tests/6_assertions.test.js
@@ -10,16 +10,19 @@ const SETTINGS = {
 
 describe('test number of elements on the site and text with assertions', () => {
 
-    it('has correct h1 text', async() => {
+    it('has correct h1 text, url and number of p nodes', async() => {
         const browser = await puppeteer.launch(SETTINGS);
         const page = await browser.newPage();
         await page.goto(TEST_SITE_URL);
 
         const pageUrl = await page.url();
         const h1Text = await page.$eval('h1', element => element.textContent);
+
+        const pElementsCount = await page.$$eval('p', elements => elements.length)
  
         expect(h1Text).to.be.a('string', 'Example'); // Type and content assertions
         expect( pageUrl).to.include('/example')
+        expect(pElementsCount).to.be.a('number', 9)
 
         await browser.close();
     })
